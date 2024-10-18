a520a341b801614d41381be51b899a83f30660f2
DapperDeBG <158341732+DapperDeBG@users.noreply.github.com>
2024-10-18 12:47:01 +0000

    datatypes

---
+++ .devcontainer/basics_01/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")   // we are using nodejs, not browser
+
+/*
+//data types
+-- primitive type --
+//number => max range: 2 to the power 53
+//bigInt = very very big number,generally not used that much
+//string => " " double/single quote
+//boolean => true/false
+//null => standalone value, special type
+//undefined => value not assigned yet
+//symbol => unique    */
+
+
+console.log(typeof "DebD"); //string
+
+console.log(typeof null ); //object   
+console.log(typeof undefined ); //undefined
+
