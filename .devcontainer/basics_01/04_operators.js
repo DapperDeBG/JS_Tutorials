37e6e9d45af3339a5f103dcc197d2685d48b6f06
DapperDeBG <158341732+DapperDeBG@users.noreply.github.com>
2024-10-22 08:48:29 +0000

    operators

---
+++ .devcontainer/basics_01/04_operators.js
@@ -0,0 +1,146 @@
+// Operators in JS
+// Used to perform some opearation on data
+
+// Arithmetic Operators
+// +,-,*,/,Modulus,Exponentiation
+
+/*let a = 5;
+let b = 2;
+
+console.log("a = ",a, " & b =",b);
+console.log(" a + b = ", a + b);
+console.log(" a - b = ", a - b);
+console.log(" a * b = ", a * b);
+console.log(" a / b = ", a / b);
+console.log(" a % b = ", a % b);   //modulus //1
+console.log(" a ** b = ", a ** b); //exponentiation //25    */
+
+//Unary Operators
+//Increment ++,Decrement--
+/*
+let a = 5;
+let b = 2;
+
+//post increment op
+// In the post-increment,value is first used in an expression and then incremented.
+// console.log("a = ",a, " & b =",b);
+// console.log("a++ = ",a++);       //a = a + 1
+// console.log("a = ", a);  //6
+
+
+
+// console.log("a = ",a, " & b =",b);
+// console.log("a-- = ",a--);        //a = a - 1
+// console.log("a = ", a); //4
+
+//pre increment op
+//In the pre-increment,value is first incremented and then used in inside the expression
+// console.log("a = ",a, " & b =",b);
+// console.log("++a = ",++a);
+// console.log("a = ",a);
+
+// console.log("a = ",a, " & b =",b);
+// console.log("--a = ",--a);
+// console.log("a = ",a);            */
+
+//Assignment Operators
+
+// left = right 
+// values always propagate from right to left in JS
+
+/*
+let a = 5;
+let b = 2;
+
+a += 4 // a = a + 4  //9
+a -= 4 // a = a - 4 //1
+a *= 4 // a = a * 4 //20
+a /= 4 // a = a / 4 //1.25
+a %= 4 // a = a % 4 //1
+a **= 4 // a = a ** 4 // 625   */
+
+
+//Comparison Operators
+/*
+// Equal to ==
+// Equal to and data type ===
+// Not Equal to !=
+// Not Equal to and data type !==
+// > , >= , < , <=
+
+// let a = 5;
+// let b = 2;
+// console.log("5 == 2", a == b); //false
+
+// console.log("5 != 2", a != b ); //true
+
+// let a1 = 5; //number
+// let b1 = "5"; //string -> number
+
+// console.log("a1 == b1", a1 == b1); //true
+
+
+// Strict version   //value + datatype both check explicitly
+
+ let a = 5;   //number
+ let b = "5"; //string
+ console.log("5 === 2", a === b); //false
+
+ let a1 = 5;
+ let b1 = 2;
+ console.log("5 !== 2", a1 !== b1); //true    */
+
+
+
+ //Logical Operators
+/*
+//  Logical AND &&
+//  Logical 0R ||
+//  Logical Not !
+
+// let a = 6;
+// let b = 5;
+// let cond1 = a > b;  //true
+// let cond2 = a === b; //false
+// console.log("cond1 && cond2 = ", cond1 && cond2); //false 
+
+// both conditions should be true in this case. If anyone is false, the result will be false.. //AND
+
+let a = 6;
+let b = 5;
+let cond1 = a < b;  //false
+let cond2 = a === 6; //true
+console.log("cond1 || cond2 = ", cond1 || cond2); //true 
+
+
+// Atleast one condition should be true in this case. //OR
+
+
+
+console.log("!(6 < 5) ", !(a < b))  //true  //NOT //opposite of false    */
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
