0b738696ca03070a5439cadbc5a12b3d8aebe1e1
DapperDeBG <158341732+DapperDeBG@users.noreply.github.com>
2024-10-23 09:00:09 +0000

    Conditional Statements_if-else ladder

---
+++ .devcontainer/basics_01/05_conditionalStatements.js
@@ -0,0 +1,113 @@
+// Conditional Statements
+// To implement some conditions in the code
+
+// IF Statement
+
+// let age =25;
+
+// if(age > 18){
+//  console.log("you can vote");
+// }
+
+// if(age < 18) {
+//  console.log("you can vote");
+// }
+
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black";
+// }
+
+// if(mode ==="light"){
+//     color = "White";
+// }
+
+// console.log(color);
+
+
+
+//If-else Statement
+
+// let mode = "blue";
+// let color;
+
+// if(mode === "dark"){
+// color = "black";
+// }
+// else{
+//     color = "white";
+// }
+
+// console.log(color);
+
+
+//odd even check
+
+// let num = 10;
+//  if (num%2 === 0) {
+//     console.log(num, "is even");
+//  }
+//  else{
+//     console.log(num, "is odd");
+//  }
+
+
+// Else-if Statement  // for checking(more then 2)many statements in one go
+
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black";
+// } else if( mode === "blue"){
+//     color = "blue"
+// } else if( mode === "pink"){
+//     color = "pink";
+// } else {
+//     color = "white"
+// }
+// console.log(color);
+
+
+// Ternary Operator  // handles 3 operands in one go // ternary op: compact if else
+
+// let age = 25;
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result);   //adult
+
+
+//Switch case statement // substitute of If-else ladder
+
+// const cups = 23;
+// switch (cups) {
+//     case 41:
+//         console.log("The value of cups is 41");
+//         break;
+//     case 42:
+//         console.log("The value of cups is 42");
+//         break;
+//     case 43:
+//         console.log("The value of cups is 43");
+//         break;
+//     case 44:
+//         console.log("The value of cups is 44");
+//         break;
+
+//     default:
+//         console.log("The value of cups is none of 41,42,42,44")
+//         break;
+// }
+
+//if we don't apply 'break' here, all the cases will get execute even after getting a proper match..
+
+
+
+ 
+
+
+
+
